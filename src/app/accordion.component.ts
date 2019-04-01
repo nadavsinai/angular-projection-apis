c6795a7e5c89ed2090ce477696e6c455b7c5de88
Nadav Sinai <nadav@algotec.co.il>
2019-04-01 15:15:22 +0300

    explain view/content children

--- src/app/accordion.component.ts
+++ src/app/accordion.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   template: `
-    <p>
+    <p #viewStuff>
       accordion works!
     </p>
     <ng-content select="h3"></ng-content>
     <ng-content select=".stuff"></ng-content>
-    
+
     <ng-content></ng-content>
   `,
   styles: []
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent implements OnInit, AfterViewInit, AfterContentInit {
+  @ViewChild('viewStuff') viewStuff: ElementRef;
+  @ContentChild('h3') projectedStuff: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
+
+  ngAfterContentInit() {
+    console.log(this.projectedStuff);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.viewStuff);
+  }
+
 
 }
