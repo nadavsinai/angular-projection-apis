5238e9b55fb0ca6bbe6d9ba70ff1c6329e2f4730
Nadav Sinai <nadav@algotec.co.il>
2019-04-01 15:05:04 +0300

    add accordion component

---
+++ src/app/accordion.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  template: `
+    <p>
+      accordion works!
+    </p>
+  `,
+  styles: []
+})
+export class AccordionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
