649020b399d2c7297b05c08e923da1231d2c7ce5
Nadav Sinai <nadav@algotec.co.il>
2019-04-01 15:19:20 +0300

    add open/close behavior

--- src/app/acc-node.component.ts
+++ src/app/acc-node.component.ts
@@ -1,13 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ContentChild, ElementRef, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-acc-node',
   template: `
-    <ng-content></ng-content>
+    <h1 (click)="swapState()">
+      <ng-container *ngTemplateOutlet="titleTemplate">></ng-container>
+    </h1>
+    <div class="wrapper" *ngIf="expanded">
+      <ng-content></ng-content>
+    </div>
+    <ng-template #default>
+      Click to Expand
+    </ng-template>
   `,
-  styles: []
+  styles: [`
+    :host {
+      display: block;
+    }
+
+    h1 {
+      color: red;
+      cursor: pointer;
+    }
+
+  `]
 })
 export class AccNodeComponent implements OnInit {
+  @Input() expanded = false;
+  @Output() nodeClicked = new EventEmitter<AccNodeComponent>();
+  @ViewChild('default', {read: ElementRef}) defaultTitle: ElementRef<any>;
+  @ContentChild('accTitle') titleTemplate: TemplateRef<any>;
+
+  swapState() {
+    this.expanded = !this.expanded;
+    this.nodeClicked.emit(this);
+  }
 
   constructor() {
   }
